cb45428625400d0e7f761034aa3a418680e28fa4
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-12-10 12:33:13 +0530

    RecentlyPlayedTrack - added some styling

--- components/recentlyPlayedTrack.js
+++ components/recentlyPlayedTrack.js
@@ -2,16 +2,19 @@
         // accessing props using destructuring
 function RecentlyPlayedTrack({ track }) {
     return (
-        <div className="text-white">
+        <div className="border-2 border-blue-900 flex items-center space-x-3">
             
             <img 
                 src={track.albumUrl}
                 alt="" 
+                className="w-[3.25rem] h[3.25rem] rounded-full"
             />
 
             <div>
-                <h4> {track.title} </h4>
-                <p> {track.artist} </p>
+                <h4 className="text-white text-[0.8rem] mb-0.5 font-semibold hover:underline cursor-pointer truncate max-w-[9.3rem]"> 
+                    {track.title} 
+                </h4>
+                <p className="text-xs text-[#686868] font-semibold hover:underline cursor-pointer"> {track.artist} </p>
             </div>
 
         </div>
