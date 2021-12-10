e9732a02d3a2c7d4a0280a5045da1b5977ef38ad
kapil551 <59477738+kapil551@users.noreply.github.com>
2021-12-10 11:33:18 +0530

    RightSidebar - added structure/outline of the rightSidebar component

--- components/rightSidebar.js
+++ components/rightSidebar.js
@@ -1,8 +1,54 @@
+import { ViewGridIcon } from "@heroicons/react/solid";
+import { HiOutlineShieldCheck } from "react-icons/hi";
+import { MdOutlineSettings } from "react-icons/md";
+import { BiBell } from "react-icons/bi";
+
 function RightSidebar() {
     return (
-        <div className="border-2 text-white">
-            Right Sidebar
-        </div>
+        <section className="border-2 text-white">
+           
+           <div className="">
+
+               {/* Icons */}
+               <div className="">
+                   
+                   <HiOutlineShieldCheck className="" />
+                   <MdOutlineSettings className="" />
+                   
+                   <div className="">
+                       <BiBell className=""/>
+                   </div>
+
+               </div>
+
+                {/* Profile */}
+                {/* <Dropdown /> */}
+
+           </div>
+
+           {/* recently played tracks */}
+           <div className="">
+
+               <div className="">
+
+                   <h4 className=""></h4>
+                   <ViewGridIcon className="" />
+
+               </div>
+
+               <div className="">
+
+                   {/* <RecentlyPlayed /> */}
+
+               </div>
+
+               <button className=""> 
+                   View All
+               </button>
+
+           </div>
+
+        </section>
     )
 }
 
